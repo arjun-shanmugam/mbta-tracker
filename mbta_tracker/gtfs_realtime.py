1a18e3a17ff55a93be564f7cf70191031b6347a2
Shanmugam <ashanmu1@dhcp-10-250-202-239.harvard.edu>
2023-07-25 13:19:59 -0700

    pull vehicle positions from gtfs-realtime

--- mbta_tracker/gtfs_realtime.py
+++ mbta_tracker/gtfs_realtime.py
@@ -1,19 +1,18 @@
+import pandas as pd
 import requests
-from typing import Union, List
 import time
 from protobuf_to_dict import protobuf_to_dict
 from google.transit import gtfs_realtime_pb2
-from joblib import Parallel, delayed
 
 
 def get_single_train_position(feed_entity):
-    if feed_entity['vehicle']['trip']['route_id'] in ["Blue", "Red", "Orange", "Green-B", "Green-C", "Green-D", "Green-B"]:
-        return {'train_id': feed_entity['id'],
-                'route_id': feed_entity['vehicle']['trip']['route_id'],
-                'longitude': feed_entity['vehicle']['position']['longitude'],
-                'latitude': feed_entity['vehicle']['position']['latitude']}
-    else:
-        return {}
+    return {'train_id': feed_entity['id'],
+            'route_id': feed_entity['vehicle']['trip']['route_id'],
+            'stop_id': feed_entity['vehicle']['stop_id'],
+            'longitude': feed_entity['vehicle']['position']['longitude'],
+            'latitude': feed_entity['vehicle']['position']['latitude']}
+
+
 class GTFSRealtime:
     _url: str = "https://api-v3.mbta.com"
     _MBTA_API_KEY: str
@@ -23,17 +22,12 @@ class GTFSRealtime:
         self._url = url
         self._feed = gtfs_realtime_pb2.FeedMessage()
 
-
-
     def get_train_positions(self):
         response = requests.get(self._url)
         self._feed.ParseFromString(response.content)
         feed = protobuf_to_dict(self._feed)
-        t1 = time.time()
-        # positions = Parallel(n_jobs=1)(delayed(get_single_train_position)(feed_entity) for feed_entity in feed['entity'])
-        positions = [get_single_train_position(feed_entity) for feed_entity in feed['entity']]
-        t2 = time.time()
-        print(t2-t1)
-        print(positions)
+        positions = [get_single_train_position(feed_entity) for feed_entity in feed['entity'] if
+                     feed_entity['vehicle']['trip']['route_id'] in ["Blue", "Red", "Orange", "Green-B", "Green-C", "Green-D",
+                                                           "Green-B"]]
 
-
+        return pd.DataFrame.from_records(positions)
